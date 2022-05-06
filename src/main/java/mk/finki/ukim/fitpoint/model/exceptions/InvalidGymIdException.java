81965e5f607f41c3627b0187501e531a6261b6c8
Martin's <marko.hoff123@gmail.com>
2022-05-06 13:11:36 +0200

    pocetok 2

--- src/main/java/mk/finki/ukim/fitpoint/model/exceptions/InvalidGymIdException.java
+++ src/main/java/mk/finki/ukim/fitpoint/model/exceptions/InvalidGymIdException.java
@@ -3,9 +3,11 @@ package mk.finki.ukim.fitpoint.model.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.function.Supplier;
+
 @ResponseStatus(code = HttpStatus.NOT_FOUND)
-public class InvalidGymIdException extends RuntimeException{
-    public InvalidGymIdException(Long id) {
-        super(String.format("Gym with id: %d is not found", id));
+public class InvalidGymIdException extends RuntimeException {
+    public InvalidGymIdException() {
+        super(String.format("Gym with id: %d is not found"));
     }
 }
