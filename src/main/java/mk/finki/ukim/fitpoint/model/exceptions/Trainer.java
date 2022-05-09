da500b4e6daceae78fa5fa66eec17cec8548dfd0
Magdalena <janevska.magdalena19@gmail.com>
2022-05-09 17:09:15 +0200

    trainer

---
+++ src/main/java/mk/finki/ukim/fitpoint/model/exceptions/Trainer.java
@@ -0,0 +1,29 @@
+package mk.finki.ukim.fitpoint.model.exceptions;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Trainer {
+    private Long id;
+    private String name;
+    private String username;
+    private String password;
+    private String number;
+    private TrainerSex sex;
+    private TrainerType type;
+
+    private Gym gym;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+}
+
